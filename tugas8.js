997a5daeece3e63930e40d24f8156f9c7d2bf68f
Hanif-Niomic <m.alihanif249@gmail.com>
2021-12-18 19:57:13 +0700

    Tugas 8 Js
    Tugas 8 Js

---
+++ tugas8.js
@@ -0,0 +1,20 @@
+var tugas8 = {
+  tanggal: 18,
+  bulan: "Desember",
+  tahun: 2021,
+}
+console.log("Nilai Awal");
+console.log(tugas8.tanggal);
+console.log(tugas8.bulan);
+console.log(tugas8.tahun);
+
+
+tugas8.tanggal = 21;
+tugas8.bulan = "Januari";
+tugas8.tahun = 2022;
+console.log("");
+
+console.log("Nilai Diubah");
+console.log(tugas8.tanggal);
+console.log(tugas8.bulan);
+console.log(tugas8.tahun);
